c65c3d44b12d6489d71c45f4147f90f9d656a000
Pulkit <singhpulkit79@gmail.com>
2025-02-02 17:17:38 +0530

    file path fixed

--- pages/2_🚦_Traffic_News.py
+++ pages/2_🚦_Traffic_News.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import pandas as pd
 
-st.image("assets/india-airports-map.png")
+st.image("./assets/india-airports-map.png")
 
 st.header("Total Passengers Movements")
 
-df1 = pd.read_excel("assets/Mar2k24Annex3 (1).xlsx",
+df1 = pd.read_excel("./assets/Mar2k24Annex3 (1).xlsx",
                     sheet_name="Table 4",
                     engine="openpyxl",
                     skiprows=2
@@ -15,7 +15,7 @@ st.dataframe(df1)
 
 st.header("Total Aircrafts Movements")
 
-df2 = pd.read_excel("assets/Mar2k24Annex2 2.xlsx",
+df2 = pd.read_excel("./assets/Mar2k24Annex2 2.xlsx",
                     sheet_name="Table 4",
                     engine="openpyxl",
                     skiprows=2
--- pages/3_🛫_RCS(Regional Connectivity Scheme).py
+++ pages/3_🛫_RCS(Regional Connectivity Scheme).py
@@ -7,14 +7,14 @@ selected = st.selectbox(
     'Select', [1.0, 2.0, 3.0, 4.0, 4.1, 4.2, 4.3, 5.0, 5.1, 5.3])
 
 if selected == 1.0:
-    df = pd.read_excel("assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
+    df = pd.read_excel("./assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
                        sheet_name="Table 1",
 
                        )
     st.dataframe(df)
 
 elif selected == 2.0:
-    df = pd.read_excel("assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
+    df = pd.read_excel("./assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
                        sheet_name="Table 2",
                        # engine="openpyxl",
                        skiprows=1
@@ -22,7 +22,7 @@ elif selected == 2.0:
     st.dataframe(df)
 
 elif selected == 3.0:
-    df = pd.read_excel("assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
+    df = pd.read_excel("./assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
                        sheet_name="Table 3",
                        # engine="openpyxl",
                        skiprows=1
@@ -30,14 +30,14 @@ elif selected == 3.0:
     st.dataframe(df)
 
 elif selected == 4.0:
-    df = pd.read_excel("assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
+    df = pd.read_excel("./assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
                        sheet_name="Table 4",
                        # engine="openpyxl"
                        )
     st.dataframe(df)
 
 elif selected == 4.1:
-    df = pd.read_excel("assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
+    df = pd.read_excel("./assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
                        sheet_name="Table 5",
                        # engine="openpyxl",
                        skiprows=1
@@ -45,7 +45,7 @@ elif selected == 4.1:
     st.dataframe(df)
 
 elif selected == 4.2:
-    df = pd.read_excel("assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
+    df = pd.read_excel("./assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
                        sheet_name="Table 6",
                        # engine="openpyxl",
                        skiprows=1
@@ -53,7 +53,7 @@ elif selected == 4.2:
     st.dataframe(df)
 
 elif selected == 4.3:
-    df = pd.read_excel("assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
+    df = pd.read_excel("./assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
                        sheet_name="Table 7",
                        # engine="openpyxl",
                        skiprows=1
@@ -61,14 +61,14 @@ elif selected == 4.3:
     st.dataframe(df)
 
 elif selected == 5.0:
-    df = pd.read_excel("assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
+    df = pd.read_excel("./assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
                        sheet_name="Table 8",
                        # engine="openpyxl"
                        )
     st.dataframe(df)
 
 elif selected == 5.1:
-    df = pd.read_excel("assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
+    df = pd.read_excel("./assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
                        sheet_name="Table 9",
                        # engine="openpyxl",
                        skiprows=1,
@@ -77,7 +77,7 @@ elif selected == 5.1:
     st.dataframe(df)
 
 elif selected == 5.3:
-    df = pd.read_excel("assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
+    df = pd.read_excel("./assets/579 RCS Routes Operationalised as on 08.07.2024.xlsx",
                        sheet_name="Table 10",
                        # engine="openpyxl",
                        skiprows=1,
